46936f7120428f07ef3aa0e240da1cf2b567fcf6
subrat <subratj611@gmail.com>
2023-07-03 22:36:58 +0530

    style: style update

--- atoms/Button/OpButton.style.js
+++ atoms/Button/OpButton.style.js
@@ -2,28 +2,32 @@ import { Button } from "@mui/material";
 import { styled } from "@mui/system";
 
 const getVariant = (variant) => {
-  variant === "contained" && "#0F6CBD";
-  variant === "outlined" && "#FFFFFF";
+  if (variant === "contained") return "#0F6CBD";
+  if (variant === "outlined") return "#FFFFFF";
 };
 
 const getColorForVariant = (variant) => {
-  variant === "contained" && "#FFFFFF";
-  variant === "outlined" && "#242424";
+  if (variant === "contained") return "#FFFFFF";
+  if (variant === "outlined") return "#242424";
 };
 
 // Styled button
-export const StyledButton = styled(Button)(({ theme, variant }) => ({
-  textTransform: "inherit",
-  fontWeight: 600,
-  background: getVariant(variant),
-  color: getColorForVariant(variant),
-  boxShadow: "none",
-  borderRadius: 4,
-  border:
-    variant === "outlined" ? "1px solid #D1D1D1" : "1px solid transparent",
-
-  "&:hover": {
+export const StyledButton = styled(Button)(
+  ({ theme, variant, mRight = "0px" }) => ({
+    textTransform: "inherit",
+    fontWeight: 600,
+    background: getVariant(variant),
+    color: getColorForVariant(variant),
     boxShadow: "none",
-    background: variant === "contained" ? "#115EA3" : "transparent",
-  },
-}));
+    borderRadius: 4,
+    marginRight: mRight,
+    border:
+      variant === "outlined" ? "1px solid #D1D1D1" : "1px solid transparent",
+
+    "&:hover": {
+      boxShadow: "none",
+      background: variant === "contained" ? "#115EA3" : "transparent",
+      border: variant === "outlined" && "1px solid #242424",
+    },
+  })
+);
