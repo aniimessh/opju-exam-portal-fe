f32fa8816fcbdcf65fa82b1fe30c30b236bb9780
subrat <subratj611@gmail.com>
2023-07-09 02:27:23 +0530

    style: width and height prop update

--- atoms/Button/OpButton.style.js
+++ atoms/Button/OpButton.style.js
@@ -13,34 +13,39 @@ const getColorForVariant = (variant) => {
 };
 
 // Styled button (default)
-export const StyledButton = styled(Button)(({ variant, mright = "0px" }) => ({
-  textTransform: "inherit",
-  fontWeight: 600,
-  background: getVariant(variant),
-  color: getColorForVariant(variant),
-  boxShadow: "none",
-  borderRadius: 4,
-  marginRight: mright,
-  border:
-    variant === "outlined" ? "1px solid #D1D1D1" : "1px solid transparent",
-  transition: "transform 200ms linear",
-
-  "&:hover": {
+export const StyledButton = styled(Button)(
+  ({ variant, mright = "0px", width = "auto", height = "auto" }) => ({
+    textTransform: "inherit",
+    fontWeight: 600,
+    background: getVariant(variant),
+    color: getColorForVariant(variant),
     boxShadow: "none",
-    background: variant === "contained" ? "#0F6CBD" : "transparent",
-    border: variant === "outlined" && "1px solid #D1D1D1",
-  },
+    borderRadius: 4,
+    marginRight: mright,
+    width: width,
+    height: height,
+    border:
+      variant === "outlined" ? "1px solid #D1D1D1" : "1px solid transparent",
+    transition: "transform 200ms linear",
 
-  "&.active": {
-    transform: "scale(0.8)",
-  },
-}));
+    "&:hover": {
+      boxShadow: "none",
+      background: variant === "contained" ? "#0F6CBD" : "transparent",
+      border: variant === "outlined" && "1px solid #D1D1D1",
+    },
+
+    "&.active": {
+      transform: "scale(0.8)",
+    },
+  })
+);
 
 // Styled button (Loading)
 export const OpLoadingButton = styled(LoadingButton)(
-  ({ width, mright = "0px" }) => ({
+  ({ width = "auto", height = "auto", mright = "0px" }) => ({
     padding: "1.15rem 0",
     width: width,
+    height: height,
     borderRadius: 4,
     marginRight: mright,
   })
